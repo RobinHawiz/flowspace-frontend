34016d4750d3dd48d3e66e352a8d4b02b3f1fbc4
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-04-17 13:25:42 +0200

    style(workspaces): make workspace card behave like a button

--- src/routes/protected/workspaces/components/WorkspaceCard.tsx
+++ src/routes/protected/workspaces/components/WorkspaceCard.tsx
@@ -6,7 +6,7 @@ type Props = WorkspaceResponse;
 
 function WorkspaceCard({ id, title }: Props) {
   return (
-    <article className="relative flex items-center justify-between rounded-lg bg-white px-4 py-2.5 shadow-sm transition-all duration-200 ease-in-out hover:bg-slate-100">
+    <article className="btn relative flex items-center justify-between rounded-lg border-none bg-white px-4 py-2.5 shadow-sm hover:bg-slate-100">
       <div className="flex gap-2.5">
         <img src={workspace} alt="" />
         <h2 className="xs:text-xl text-lg font-bold">{title}</h2>
@@ -14,6 +14,7 @@ function WorkspaceCard({ id, title }: Props) {
       <Link
         to={`/workspaces/${id}`}
         aria-label={`View ${title} workspace page`}
+        draggable={false}
         className="focus-visible:outline-accent absolute top-0 left-0 h-full w-full rounded-lg focus-visible:outline-2 focus-visible:outline-offset-2"
       ></Link>
     </article>
