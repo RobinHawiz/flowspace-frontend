ee3c427b62b82c37534f1d7b709391edd52f09ab
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2026-04-23 14:34:14 +0200

    style(workspaces): prevent columns from growing too wide

--- src/routes/protected/workspace/components/WorkspaceColumn.tsx
+++ src/routes/protected/workspace/components/WorkspaceColumn.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function WorkspaceColumn({ workspaceColumn }: Props) {
   return (
-    <div className="border-accent min-w-70 rounded-lg border border-solid bg-white/28 p-2.5 shadow-md backdrop-blur-sm">
+    <div className="border-accent w-full max-w-70 rounded-lg border border-solid bg-white/28 p-2.5 shadow-md backdrop-blur-sm">
       <h2 className="mb-3 pl-2.5 text-lg font-bold">{workspaceColumn.title}</h2>
       {/* Placeholder task content, replace with actual tasks when implemented. */}
       <ul className="flex flex-col gap-3">
